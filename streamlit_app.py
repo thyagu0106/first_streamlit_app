9d3c7c534c5d1a912f8d3607f7e9b92328955c1c
thyagu0106 <39317096+thyagu0106@users.noreply.github.com>
2023-03-31 18:51:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 
 import streamlit
 streamlit.title('Aasini-First Robotic project')
+streamlit.title('She won Best team award')
+streamlit.title('Aasini selected for State compitition')
+streamlit.title('Aasini went to Iowa to compete')
+streamlit.title('Aasini select for world compitition')
+streamlit.title('Aasini going to Dallas for world Championship !!')
+
