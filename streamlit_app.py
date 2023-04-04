55c9368e245969eee86d7f21a9d453de12e884bd
thyagu0106 <39317096+thyagu0106@users.noreply.github.com>
2023-04-03 22:01:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,7 @@ streamlit.header('🥑Breakfast Menu🥑')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal🍞')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg 🐔 ')
-
- streamlit.text('Import your own fruit List')
+streamlit.text('Import your own fruit List')
  
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
