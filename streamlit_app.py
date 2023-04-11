31cd9f314003a5eb2edca0525dba37dde093b024
thyagu0106 <39317096+thyagu0106@users.noreply.github.com>
2023-04-10 20:28:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,21 +36,25 @@ streamlit.header('Selected Fruits...')
 streamlit.dataframe(fruits_to_show)
 
 # import Python packasge
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.header('Fruitvice Advice!')
+
 # streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
+streamlit.header('Fruitvice Fruit Advice!')
+try:
+ fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+ if not fruit_choice:
+  streamlit.error("Please select a fruit of your choice to get information!")
+ else:
+  streamlit.write('The user entered ', fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)    
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
+ except URLError as e:
+  streamlit.error()
+  
 streamlit.stop()
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
