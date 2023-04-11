6d61e581b388c46e6211b7cc95c2aa3587c3b825
thyagu0106 <39317096+thyagu0106@users.noreply.github.com>
2023-04-10 23:37:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,21 +52,18 @@ try:
 except URLError as e:
 	streamlit.error()
   
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-my_fruit = my_cnx.cursor()
-my_fruit.execute("SELECT * from fruit_load_list")
-my_data_row = my_fruit.fetchall()
-streamlit.text("Fruit List:")
-streamlit.dataframe(my_data_row)
-
+#streamlit.stop()
+#Snowflake Related functions
+def get_fruit_load_list():
+	with my_cnx.cursor() as my_cur:
+		my_cur.execute("select * from fruit_load_list")
+		return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+	my_data_rows = get_fruit_load_list()
+	streamlit.dataframe(my_data_rows)
+	
 # import streamlit as st
 
 add_my_fruit = streamlit.text_input('What fruit would like to add?', 'Koyya')
